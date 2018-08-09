63ec159ac21e69dd7a8c67707400264ed84d99b6
Marcin Andrzejewski <andrzejewski.m4@gmail.com>
2018-08-09 17:35:29 +0200

    eslint added
    changes in app state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 import MapContainer from './components/MapContainer'
 import locations from './Data/locations'
@@ -7,92 +6,106 @@ import locations from './Data/locations'
 
 class App extends Component {
 
-  state = {
-    showingInfoWindow: false,
-    activeMarker: {},
-    selectedPlace: {},
-    error: false,
-    locations: [],
-    filteredLocations: [],
-    query: '',
-    selectLocationId: '',
-    locations: locations,
-  };
-
-  onPinClick = (props, marker, e) =>
-    this.setState({
-      showingInfoWindow: true,
-      activeMarker: marker,
-      selectedPlace: props,
-    });
-
-  onMapClicked = (props) => {
-    if (this.state.showingInfoWindow) {
-      this.setState({
+    state = {
         showingInfoWindow: false,
-        activeMarker: null,
-      })
-    }
-  };
+        activeMarker: {},
+        selectedPlace: {},
+        error: false,
+        filteredLocations: [],
+        query: '',
+        selectLocationId: '',
+        locations: locations,
+        photos: [],
+    };
 
-  componentDidMount() {
-    const key = "7ef1ac0ab2778bc938233edba3b4ff9c";
-    const secret = "19b4fb9bdb7a8ad7";
-    const text = ["ORP%20B%C5%81YSKAWICA", "Pierożek%20Pierogarnia", "Aquarium", "Dom%20Marynarza", "Barracuda"]
-
-    for (var item of text) {
-      fetch(
-        `https://api.flickr.com/services/rest/?method=flickr.photos.search&pages=1&per_page=10&api_key=${key}&format=json&nojsoncallback=1&text=${item}&extras=url_o`
-        //    `https://api.foursquare.com/v2/venues/explore?ll=52.2246756,21.0122287&categoryId=4bf58dd8d48988d142941735&checkin=intent&radius=6000&limit=50&client_id=${key}&client_secret=${secret}&v=20180726`
-      )
-
-        .then(response => response.json())
-        .then(res => {
-          const result = res.response;
-
-          this.setState({
-            locations: result,
-            filteredLocations: result
-          });
-        })
-        .catch(error => {
-          console.log(error);
-          this.setState({ error: true });
+    onPinClick = (props, marker, e) =>
+        this.setState({
+            showingInfoWindow: true,
+            activeMarker: marker,
+            selectedPlace: props,
         });
 
-      // HANDLE ERRORS
-      window.gm_authFailure = () => this.setState({ error: true });
-      if (window.google === undefined) {
-        this.setState({ error: true });
-      }
+    onInfoWindowClose = (props) =>
+        this.setState({
+            showingInfoWindow: false,
+            activeMarker: null,
+            selectedPlace: null,
+        });
+
+
+    onMapClicked = (props) => {
+        if (this.state.showingInfoWindow) {
+            this.setState({
+                showingInfoWindow: false,
+                activeMarker: null,
+            })
+        }
+    };
+
+
+    componentDidMount() {
+        const key = "7ef1ac0ab2778bc938233edba3b4ff9c";
+        const secret = "19b4fb9bdb7a8ad7";
+        const text = ["ORP%20B%C5%81YSKAWICA", "Pierożek%20Pierogarnia", "Aquarium", "Dom%20Marynarza", "Barracuda"];
+
+        for (var item of text) {
+            fetch(
+                `https://api.flickr.com/services/rest/?method=flickr.photos.search&pages=1&per_page=10&api_key=${key}&format=json&nojsoncallback=1&text=${item}&extras=url_o`
+                //    `https://api.foursquare.com/v2/venues/explore?ll=52.2246756,21.0122287&categoryId=4bf58dd8d48988d142941735&checkin=intent&radius=6000&limit=50&client_id=${key}&client_secret=${secret}&v=20180726`
+            )
+
+                .then(response => response.json())
+                .then(res => {
+                    const result = res.response;
+
+                    this.setState({
+                        photos: result,
+                        // filteredLocations: result
+
+                    });
+                    console.log(this.state.photos);
+                })
+                .catch(error => {
+                    console.log(error);
+                    this.setState({error: true});
+                });
+
+            // HANDLE ERRORS
+            window.gm_authFailure = () => this.setState({error: true});
+            if (window.google === undefined) {
+                this.setState({error: true});
+            }
+        }
     }
-  }
 
-  render() {
-    let locations = this.state.locations;
+    render() {
+        let locations = this.state.locations;
 
-    return (
-      <div className="App">
-        <header className="App-header">
-         {/* <Button/> */}
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          <div className="map" role="application">
-            <MapContainer
-              google={window.google}
-              addMarker={this.locations}
-              onPinClick={this.onPinClick}
-              onInfoWindowClose={this.onInfoWindowClose}
-              onMapClick={this.onMapClick}
-              appState={this.state}
-            />
+        return (
+            <div className="App">
+                <header className="App-header">
+                    {/* <menuButton/> */}
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    <div className="map" role="application">
+                        <MapContainer
+                            google={window.google}
+                            addMarker={this.locations}
+                            onClick={this.props.onPinClick}
+                            onInfoWindowClose={this.onInfoWindowClose}
+                            onMapClick={this.onMapClick}
+                            appState={this.state}
+                            marker={this.props.activeMarker}
+                        />
 
-          </div>
-        </p>
-      </div>
-    );
-  }
+
+                    </div>
+                </p>
+            </div>
+        );
+    }
 }
+
 // APIkey = AIzaSyDF1sMgvToCsxgaeFVp49tGp0_5jJv4jTU;
 export default App;
--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,62 +1,55 @@
-import React, { Component } from 'react'
-import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react'
+import React, {Component} from 'react'
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react'
 import locations from '../Data/locations'
 
 export class MapContainer extends Component {
 
-
     render() {
- 
-        
         const style = {
             width: '90%',
-            height: '90%'
-        }
+            height: '90%',
+            margin: 'auto',
+        };
+
         return (
             <Map google={this.props.google}
-                style={style}
-                zoom={12}
-                onClick={this.onMapClicked}
-                initialCenter={{
-                    lat: 54.5188898,
-                    lng: 18.5305409
-                }}
+                 style={style}
+                 className={'map'}
+                 zoom={12}
+                 onClick={this.onMapClicked}
+                 initialCenter={{
+                     lat: 54.5188898,
+                     lng: 18.5305409
+                 }}
             >
-               
+
                 <Marker onClick={this.props.onPinClick}
-                    name={'Gdynia'} />
+                        name={'Gdynia'}/>
                 <Marker
                     onClick={this.onPinClick}
-                    name={'Current location'}
                     title={locations[0].name}
                     name={locations[0].name}
-                    position={locations[0].location} />
+                    position={locations[0].location}/>
                 <Marker
                     onClick={this.onPinClick}
-                    name={'Current location'}
-                    onClick={this.onPinClick}
-                    name={'Current location'}
                     title={locations[1].name}
                     name={locations[1].name}
-                    position={locations[1].location} />
+                    position={locations[1].location}/>
                 <Marker
                     onClick={this.onPinClick}
-                    name={'Current location'}
                     title={locations[2].name}
                     name={locations[2].name}
-                    position={locations[2].location} />
+                    position={locations[2].location}/>
                 <Marker
                     onClick={this.onPinClick}
-                    name={'Current location'}
                     title={locations[3].name}
                     name={locations[3].name}
-                    position={locations[3].location} />
+                    position={locations[3].location}/>
                 <Marker
                     onClick={this.onPinClick}
-                    name={'Current location'}
                     title={locations[4].name}
                     name={locations[4].name}
-                    position={locations[4].location} />
+                    position={locations[4].location}/>
 
                 <InfoWindow
                     marker={this.props.activeMarker}
